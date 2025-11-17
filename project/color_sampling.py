d1b64a6ec3e0c83bab8e523ab266fb335ca7468d
mingli202 <vincentmingli@gmail.com>
2025-11-17 13:15:43 -0500

    stuff

--- project/color_sampling.py
+++ project/color_sampling.py
@@ -1,6 +1,6 @@
 from time import sleep
 from utils.brick import EV3ColorSensor, reset_brick, wait_ready_sensors, TouchSensor
-from color_sensor import ColorSensor
+from color_sensor.color_sensor import ColorSensor
 
 DELAY_SEC = 0.01  # seconds of delay between measurements
 DATA_FILE_BASE_PATH = "../../data_analysis/color_data/"
@@ -8,42 +8,49 @@ DATA_FILE_BASE_PATH = "../../data_analysis/color_data/"
 # complete this based on your hardware setup
 COLOR_SENSOR = EV3ColorSensor(1)
 TOUCH_SENSOR = TouchSensor(2)
+ANOTHER_TOUCHE = TouchSensor(3)
 
 
 def main():
-    wait_ready_sensors(True) # Input True to see what the robot is trying to initialize! False to be silent.
+    wait_ready_sensors(
+        True
+    )  # Input True to see what the robot is trying to initialize! False to be silent.
     print("Color Sensor is ready.")
     color_sensor = ColorSensor(COLOR_SENSOR)
     try:
         for color in ColorSensor.REFS.keys():
             collect_color_sensor_data(color, color_sensor)
     except BaseException:  # capture all exceptions including KeyboardInterrupt (Ctrl-C)
+        print("error")
         pass
     finally:
         reset_brick()  # Turn off everything on the brick's hardware, and reset it
         exit()
 
+
 def collect_color_sensor_data(color: str, color_sensor: ColorSensor):
     print("-=-=-=-=- Collect color data for", color, "-=-=-=-=-")
     file_output = f"{DATA_FILE_BASE_PATH}{color.lower()}.txt"
     output = []
-    
-    while True:
+    i = 0
+
+    while not ANOTHER_TOUCHE.is_pressed():
         if TOUCH_SENSOR.is_pressed():
             print("Touch sensor pressed, collecting rgb data...")
             while TOUCH_SENSOR.is_pressed():
                 r, g, b = color_sensor.get_rgb()
-                if color_sensor.filter_data(r, g, b): # If None is given, then data collection failed that time
+                if color_sensor.filter_data(
+                    r, g, b
+                ):  # If None is given, then data collection failed that time
+                    i += 1
+                    print(f"{color}: {i}")
                     output.append(f"{r}, {g}, {b}")
                 sleep(DELAY_SEC)
             print("Touch sensor released, stopping data collection.")
-            break
-
 
     with open(file_output, "w") as file:
         file.write("\n".join(output))
 
-    
 
 if __name__ == "__main__":
     main()
--- project/color_sensor/color_sensor.py
+++ project/color_sensor/color_sensor.py
@@ -1,5 +1,6 @@
 from utils.brick import EV3ColorSensor, wait_ready_sensors
 
+
 class ColorSensor:
     REFS = {
         "RED": (255.0, 0.0, 0.0),
@@ -7,19 +8,18 @@ class ColorSensor:
         "BLUE": (0.0, 0.0, 255.0),
         "YELLOW": (255.0, 255.0, 0.0),
         "BLACK": (0.0, 0.0, 0.0),
-        "WHITE": (255.0, 255.0, 255.0)
+        "WHITE": (255.0, 255.0, 255.0),
     }
 
-    NORMALIZED_REFS = {key: ColorSensor.normalize_rgb(value) for key, value in REFS.items()}
+    # NORMALIZED_REFS = {key: ColorSensor.normalize_rgb(value) for key, value in REFS.items()}
 
     def __init__(self, sensor: EV3ColorSensor):
         wait_ready_sensors()
         self.sensor = sensor
-        
 
     def get_rgb(self) -> list[float]:
         return self.sensor.get_rgb()
-    
+
     def classify_color(self) -> str:
         rgb = self.get_rgb()
         red, green, blue = rgb
@@ -32,21 +32,19 @@ class ColorSensor:
             return "blue"
         else:
             return "unknown"
-    
-    @staticmethod
-    def normalize_rgb(rgb: tuple[float]) -> tuple[float]:
+
+    def __normalize_rgb(self, rgb: list[float]) -> tuple[float, ...]:
         total = sum(rgb)
         if total == 0:
             return (0.0, 0.0, 0.0)
-        return (value / total for value in rgb)
-    
-    def filter_data(r, g, b):
+        return tuple(value / total for value in rgb)
+
+    def filter_data(self, r: float, g: float, b: float):
         if r is not None and g is not None and b is not None:
             if r > 0 and g > 0 and b > 0:
                 return True
         return False
 
-    
     def is_color_detected(self, target_color: str, threshold: float = 0.1) -> bool:
         rgb = self.get_rgb()
         normalized_rgb = self.__normalize_rgb(rgb)
@@ -60,8 +58,3 @@ class ColorSensor:
             return blue > red + threshold and blue > green + threshold
         else:
             return False
-        
-    
-    
-
-    
