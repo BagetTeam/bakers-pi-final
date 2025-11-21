0470742ff624ee9ba76d03321688a55406cb4bf0
Benjamin <benjamincf01@gmail.com>
2025-11-21 11:48:22 -0500

    gyro sensor thread

--- project/gyro_sensor/gyro_sensor.py
+++ project/gyro_sensor/gyro_sensor.py
@@ -1,16 +1,47 @@
-from utils.brick import EV3GyroSensor, wait_ready_sensors
+import math
+from threading import Thread
+from time import sleep
+from typing import Union
+from utils.brick import EV3GyroSensor
+
 
 class GyroSensor:
-    def __init__(self, sensor: EV3GyroSensor):
-        wait_ready_sensors()
-        self.sensor = sensor
-        self.sensor.set_mode("abs")
-        self.reference = 0.0
+    sensor: EV3GyroSensor
+    current_angle: float # degrees
+    cache: dict = {}
+    thread: Thread
+    thread_run: bool = True
 
-    def get_angle(self) -> float:
-        return self.sensor.get_abs_measure() - self.reference
+    def __init__(self, sensor: EV3GyroSensor):
+        print("initializing gyro sensor")
+        self.sensor = sensor
+        # self.current_angle = 
+        self.init_cache()
+                
+        self.sensor.set_mode("abs")
+        self.thread = Thread(target=self.main, args=[])
+        self.thread.start()
+        self.current_angle: float = 0.0
+
+    def init_cache(self):
+        # No cache needed for gyro sensor, but method included for consistency
+        self.cache['REF'] = self.sensor.get_abs_measure()
+    
+    def main(self):
+        while self.thread_run:
+            _ = self.__get_angle()
+            sleep(0.01)
+
+    def __get_angle(self) -> float:
+        return self.sensor.get_abs_measure() - self.cache['REF']
+    
+    def get_current_angle(self) -> float:
+        return self.current_angle
     
     def set_reference(self):
         self.reference = self.sensor.get_abs_measure()
 
-    
+    def dispose(self):
+        print("disposing gyro sensor")
+        self.thread_run = False
+        self.thread.join()
